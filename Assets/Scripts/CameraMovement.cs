ae3c3c6be6568adbb0d1451b7936ce2a1d889828
Stephen Ahmad <privat.ahmad@gmail.com>
2017-06-25 22:53:42 +0200

    Project Creation

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+
+	public GameObject Car;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var dif = Vector3.Lerp(gameObject.transform.position, Car.transform.position, 1 * Time.deltaTime);
+        dif.y = gameObject.transform.position.y;
+        gameObject.transform.position = dif;
+	}
+		
+}
